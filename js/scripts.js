1d14945893a2212b342d17b29dc924b410fd21d0
Liz Kelley and Marina Poltorak <liz@>
2019-03-26 16:01:30 -0700

    add functioning functions to scripts.js to show and hide definitions, add display:none to css to make it work

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
   $("#js-click").click(function() {
-    $("#js-click").toggle();
-    $("#js-def").toggle();
+    $("#js-def-hidden").toggle();
+    $("#js-click").show();
   });
-  $("#js-def").click(function() {
-    $("#js-def").toggle();
-    $("#js-click").toggle();
+  $("#op-click").click(function() {
+    $("#op-def-hidden").toggle();
+    $("#op-click").show();
+  });
+  $("#variables-click").click(function() {
+    $("#var-def-hidden").toggle();
+    $("#variables-click").show();
+  });
+  $("#naming-click").click(function() {
+    $("#name-def-hidden").toggle();
+    $("#naming-click").show();
   });
 });
